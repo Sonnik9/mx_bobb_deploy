0b4af2b46ce22d7545d29b1a79f4ef67eaf312a5
kali <kali@kali.netis.cc>
2025-09-10 18:26:55 +0300

    ch sleep

--- main.py
+++ main.py
@@ -325,7 +325,7 @@ class Core:
         self.context.orders_updated_event.clear()
         print("[DEBUG] Order update event cleared, entering main signal loop")
 
-        instrume_update_interval = 10.0
+        instrume_update_interval = 300.0
         last_instrume_time = time.monotonic()
 
         while not self.context.stop_bot_iteration and not self.context.stop_bot:
